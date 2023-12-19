e61a1e3c5a098eabd19b68c26803fd15c49124f8
Amal1Nandu <121293964+Amal1Nandu@users.noreply.github.com>
2023-12-19 10:48:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,27 +8,16 @@ const code = "www.google.com";
 QRCode.toDataURL(code, { errorCorrectionLevel: 'H' })
   .then(url => {
     console.log(url)
+    
+        //qr-image
+  var qr_svg = qr.image(code, { type: 'png' });
+  qr_svg.pipe (fs.createWriteStream(code + '.png'));
   })
+  
   .catch(err => {
     console.error(err)
   })
 
-// With async/await
-const generateQR = async code => {
-  try {
-    console.log(await QRCode.toDataURL(code));
-    console.log("success!")
-
-     //qr-image
-  var qr_svg = qr.image(code, { type: 'png' });
-
-  qr_svg.pipe (fs.createWriteStream(code + '.png'));
- 
-
-  } catch (error) {
-    console.error(error);
-  }
-  };
 
   generateQR(code);
   
